9fe59d9585c059aef041291e849da142fbb5c1bc
Brian Leach Christensen (Mjølner) <30732216+blcoyote@users.noreply.github.com>
2024-07-15 18:59:39 +0200

    Update auth-actions.ts

--- src/actions/auth-actions.ts
+++ src/actions/auth-actions.ts
@@ -2,17 +2,11 @@
 
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
-
 import { HOME_ROUTE, ROOT_ROUTE } from '@/constants';
 import { serverConfig } from '@/config';
 
 export async function createSession(uid: string) {
-  cookies().set(serverConfig.cookieName, uid, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    maxAge: 60 * 60 * 24 * 30, // One day
-    path: '/',
-  });
+  cookies().set(serverConfig.cookieName, uid, serverConfig.cookieSerializeOptions);
 
   redirect(HOME_ROUTE);
 }
