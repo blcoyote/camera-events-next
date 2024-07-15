a33bfdf09d485b3c294da5eeeb70cf8028231f72
blcoyote <30732216+blcoyote@users.noreply.github.com>
2024-07-15 22:22:42 +0200

    firebase messaging

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,14 +1,44 @@
-"use client";
-import { firebaseAuth } from "@/firebase";
-import HomePage from "./Homepage";
+'use client';
 
-
-
+import { useEffect, useState } from 'react';
+import HomePage from './Homepage';
+import { firebaseAuth, firebaseCloudMessaging } from '@/libs/firebase/firebase';
 
 export default function Home() {
-    
-    const user = firebaseAuth.currentUser;
-    const token = firebaseAuth.currentUser?.getIdToken();
+  const [fcmToken, setFcmToken] = useState<string | undefined>(undefined);
 
-    return <HomePage email={user?.email ?? ''} />;
+  const getToken = async () => {
+    try {
+      const token = await firebaseCloudMessaging.init();
+      if (token) {
+        await firebaseCloudMessaging.getMessage();
+        setFcmToken(token);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    navigator.serviceWorker
+      .register('/firebase-messaging-sw.js')
+      .then((registration) => console.log('Service Worker registration successful with scope: ', registration.scope))
+      .catch((err) => console.log('Service Worker registration failed: ', err));
+  }, []);
+
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.addEventListener('message', (event) =>
+        console.log('event for the service worker', event)
+      );
+    }
+    async function setToken() {
+      await getToken();
+    }
+    setToken();
+  }, []);
+
+  const user = firebaseAuth.currentUser;
+  const token = firebaseAuth.currentUser?.getIdToken();
+
+  return <HomePage email={user?.email ?? ''} />;
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import { cookies } from 'next/headers';
 import './globals.css';
 import Header from '@/components/Header';
 import { serverConfig } from '@/config';
+import { useEffect, useState } from 'react';
+import { firebaseCloudMessaging } from '@/libs/firebase/firebase';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,18 +14,18 @@ export const metadata: Metadata = {
   description: 'Nextjs version of camera events',
 };
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // added
   const session = cookies().get(serverConfig.cookieName)?.value || null;
+
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <Header session={session} /> 
-          {children}
+        <Header session={session} />
+        {children}
       </body>
     </html>
   );
