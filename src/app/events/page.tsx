4ea5bf2c1f8380e74265490d28b7c1334a8bcd50
blcoyote <30732216+blcoyote@users.noreply.github.com>
2024-07-16 18:48:25 +0200

    Update page.tsx

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -16,7 +16,18 @@ export default function Page() {
 
   useEffect(() => {
     if (fcmToken && accessToken) {
-      postFcmToken(fcmToken, accessToken);
+      fetch(`${process.env.NEXT_PUBLIC_BASEURL}/api/v2/fcm?fcm_token=${fcmToken}&token=${accessToken}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((result) => {
+          return result.json;
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
     }
   }, [fcmToken, accessToken]);
 
